3c5d391263b997554ff862c50e7657ba0ed40f42
AethiQs-Karel <karel.hernandez@aethiqs.nl>
2025-04-13 17:00:59 -0300

    Refactor server and test files: standardize database connection import and clean up code structure

--- server.js
+++ server.js
@@ -1,89 +1,59 @@
 'use strict';
 
 // .env file can hold PORT variable if desired
-require('dotenv').config();
+require("dotenv").config();
+require("./dbconnection");
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const DBConnection = require('./dbconnection.js');
 const apiRoutes = require('./routes/api.js');
 const fccTestingRoutes = require('./routes/fcctesting.js');
 const runner = require('./test-runner');
 
 const app = express();
 
-// Log incoming requests in development:
-if (process.env.RUN_MODE === 'development') {
-  app.use((req, res, next) => {
-    console.log(
-      `${req.method} ${req.path}; IP=${req.ip}; https?=${req.secure}`,
-    );
-    next();
-  });
-}
+app.use("/public", express.static(process.cwd() + "/public"));
 
-// Serve static files from /public folder on any request to /public
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(cors({ origin: "*" })); //For FCC testing purposes only
 
-app.use(cors({ origin: '*' })); // For FCC testing purposes only
-
-// Parse request JSON and url encoded bodies
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Connect to DB before connecting app routes:
-DBConnection.getClient()
-  .then(async (dbClient) => {
-    // Front end for specific project issues:
-    app.route('/:project/').get(function (req, res) {
-      res.sendFile(__dirname + '/views/issue.html');
-    });
-
-    // Serve index.html page on get request to '/'
-    app.route('/').get(function (req, res) {
-      res.sendFile(__dirname + '/views/index.html');
-    });
-
-    // For FCC testing purposes
-    fccTestingRoutes(app);
-
-    // Routing for API
-    await apiRoutes(app);
-
-    // 404 page not found:
-    app.get('*', (req, res) => {
-      console.log('HIT 404 ROUTE');
-      // Redirect to index
-      res.redirect('/');
-    });
-  })
-  .catch((err) => {
-    // If an error occurs, respond to requests with error message
-    console.error('Error when trying to set up routes with DB: ', err);
-    app.use('*', (req, res) => res.send('Database connection error!'));
-  });
-
-// Internal Error Handler:
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Internal Server error: See Server Logs');
+//Sample front-end
+app.route("/:project/").get(function (req, res) {
+  res.sendFile(process.cwd() + "/views/issue.html");
 });
 
-// Have server listen on PORT or default to 3000
-// http://localhost:3000/
-// If NODE_ENV='test' then tests will be run on startup
-const listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-  if (process.env.NODE_ENV === 'test') {
-    console.log('Running Tests...');
+//Index page (static HTML)
+app.route("/").get(function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+//For FCC testing purposes
+fccTestingRoutes(app);
+
+//Routing for API
+apiRoutes(app);
+
+//404 Not Found Middleware
+app.use(function (req, res, next) {
+  res.status(404).type("text").send("Not Found");
+});
+
+//Start our server and tests!
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Listening on port " + process.env.PORT);
+  if (process.env.NODE_ENV === "test") {
+    console.log("Running Tests...");
     setTimeout(function () {
       try {
         runner.run();
       } catch (e) {
-        console.log('Tests are not valid:');
-        console.error(e);
+        let error = e;
+        console.log("Tests are not valid:");
+        console.log(error);
       }
     }, 3500);
   }
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -2,7 +2,7 @@ const chaiHttp = require("chai-http");
 const chai = require("chai");
 const assert = chai.assert;
 const server = require("../server");
-require("../db-connection");
+require("../dbconnection");
 
 chai.use(chaiHttp);
 
